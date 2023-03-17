695545e868d8a33873a9632c3b4ad4019d0aa502
unknown <maxroyal.eisenberg@gmail.com>
2023-03-16 21:42:18 -0700

    added product columns

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -7,6 +7,24 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    inStock: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
   },
   {
     sequelize,
